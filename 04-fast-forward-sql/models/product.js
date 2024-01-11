f829b2bbb55578c43c00bd93895bad63b31080cb
dancielos <iam@dancielos.com>
2024-01-10 21:35:47 -0700

    currently learning mongoDB

--- 04-fast-forward-sql/models/product.js
+++ 04-fast-forward-sql/models/product.js
@@ -1,27 +1,78 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('./../util/database').getDb;
 
-const sequelize = require('../util/database');
+class Product {
+	constructor(title, price, description, imageUrl, id) {
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.imageUrl = imageUrl;
+		this._id = id;
+	}
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
+	// async save() {
+	// 	try {
+	// 		const db = getDb();
+	// 		let dbOp;
+	// 		if (this._id) {
+	// 			dbOp = await db
+	// 				.collection('products')
+	// 				.updateOne({ _id: new mongodb.ObjectId(this._id) }, { set: this });
+	// 		} else {
+	// 			dbOp = await db.collection('products').insertOne(this);
+	// 		}
+
+	// 		// console.log(res);
+	// 		return dbOp;
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 		throw err;
+	// 	}
+	// }
+
+	save() {
+		const db = getDb();
+		let dbOp;
+
+		if (this._id) {
+			dbOp = db
+				.collection('products')
+				.updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+		} else {
+			dbOp = db.collection('products').insertOne(this);
+		}
+
+		return dbOp
+			.then((result) => console.log(result))
+			.catch((err) => console.log(err));
+	}
+
+	static fetchAll() {
+		const db = getDb();
+		return db
+			.collection('products')
+			.find()
+			.toArray()
+			.then((products) => {
+				// console.log(products);
+				return products;
+			})
+			.catch(console.log);
+	}
+
+	static findById(id) {
+		// console.log('find by id', id);
+		const db = getDb();
+		return db
+			.collection('products')
+			.find({ _id: new mongodb.ObjectId(id) })
+			.next()
+			.then((product) => {
+				console.log(product);
+				return product;
+			})
+			.catch(console.log);
+	}
+}
 
 module.exports = Product;
--- 04-fast-forward-sql/util/database.js
+++ 04-fast-forward-sql/util/database.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = async (callback) => {
+	try {
+		const client = await MongoClient.connect(
+			'mongodb+srv://dan:ru0NE5EUJsYzKGBS@101.ywu0txw.mongodb.net/?retryWrites=true&w=majority'
+		);
+
+		console.log('Connected');
+
+		_db = client.db(); // this will keep on running
+		callback(client);
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
+};
+
+const getDb = () => {
+	if (_db) return _db;
+
+	throw 'No database found';
+};
+
+// module.exports = mongoConnect;
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
